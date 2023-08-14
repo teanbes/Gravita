f043c082baf8e2a48f3e9311c01f705979b748d1
teanbes <teanbes@gmail.com>
2023-08-14 19:12:44 -0400

    Parallax effect

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private Material material;
+    private float distance;
+
+    [Range(0f, 1f)]
+    [SerializeField] private float speed = 0.2f;
+
+    private void Start()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        distance += Time.deltaTime * speed;
+        material.SetTextureOffset("_MainTex", Vector2.right * distance);
+    }
+}
